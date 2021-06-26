bcfa404dfbd10d7208c5ce9e9973e7750b7e0f28
Bartash Yriy <66261160+uyriy18@users.noreply.github.com>
2021-06-26 13:53:01 +0300

    Добавьте файлы проекта.

---
+++ Task2/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+   
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public char Message { get; set; }
+        public void OnGet()
+        {
+            Message = getRand();
+        }
+        private char getRand()
+        {
+            Random rand = new Random();
+            return (char)rand.Next(65, 90);
+        }
+    }
+}
---
+++ Task3/Pages/Restaruants.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Task3.Pages
+{
+    public class RestaruantsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
